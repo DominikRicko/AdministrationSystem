77b24611191fe75abe690556ca042e365a5ab3cf
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-14 13:23:04 +0200

    Added JPA entity for user

--- src/main/java/com/samuraiDigital/adminsystem/AdminSystemApplication.java
+++ src/main/java/com/samuraiDigital/adminsystem/AdminSystemApplication.java
@@ -2,8 +2,10 @@ package com.samuraiDigital.adminsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication()
+@ComponentScan({"com.samuraiDigital.adminsystem"})
 public class AdminSystemApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/samuraiDigital/adminsystem/data/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.samuraiDigital.adminsystem.data.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.samuraiDigital.adminsystem.data.model.User;
+
+interface UserRepository extends CrudRepository<User, Long>{
+
+	Optional<User> findByName(String name);
+	
+}
