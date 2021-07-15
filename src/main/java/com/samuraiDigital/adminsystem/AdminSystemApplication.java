79a059b1016ffe80968cb2a02d0f99c0bd637c48
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-15 16:21:06 +0200

    Added BCrypt encryption for pw

--- src/main/java/com/samuraiDigital/adminsystem/AdminSystemApplication.java
+++ src/main/java/com/samuraiDigital/adminsystem/AdminSystemApplication.java
@@ -3,8 +3,10 @@ package com.samuraiDigital.adminsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 @EnableJpaRepositories("com.samuraiDigital.adminsystem.data.repositories")
@@ -13,7 +15,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class AdminSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdminSystemApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(AdminSystemApplication.class, args);
+		//PasswordEncoder encoder = (PasswordEncoder)ctx.getBean("passwordEncoder");
+		//System.out.println(encoder.encode("abcdefgh"));
 	}
 
 }
