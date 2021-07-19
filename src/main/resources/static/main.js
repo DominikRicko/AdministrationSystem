5efcfc9787e9c2577364959664a33f16d84c7533
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-19 10:54:30 +0200

    Added jquery and kendo UI

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/main/ts/index.ts ***!
+  \******************************/
+
+var testDiv = document.getElementById("test");
+console.log($("#test"));
+console.log(kendo);
+$("#test").kendoAlert();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
