fd16375250548d3c438671bc532aed8a526311df
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-19 15:29:54 +0200

    basic header added

--- src/main/ts/header.ts
+++ src/main/ts/header.ts
@@ -1,4 +1,17 @@
-$("#header").kendoAppBar({
-    items: [{template: "<span><input /></span>"}]
+import "../style/base.scss";
+
+$(document).ready(function(){
+    $("btnLogin").kendoButton();
 })
-console.log("Appbar generated.")
+
+// $("#header").kendoAppBar({
+//     items: [
+//         {template: "<img id='logo' src='/images/logo.png' alt='Logo'>", type: "contentItem", className: "headerLeft"},
+//         {template: "<span> logout </span>", type: "contentItem", className: "headerRight"},
+//     ],
+//     positionMode: 'sticky',
+//     position: 'top',
+//     themeColor: 'light',
+// })
+
+kendo.init('#btnLogin');
