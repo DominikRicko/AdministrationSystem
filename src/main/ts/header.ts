ac5897d359873204c3538e130f53aca2d12d76cb
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-20 15:48:44 +0200

    kendo App is file with kendo.init

--- src/main/ts/header.ts
+++ src/main/ts/header.ts
@@ -1,15 +1,1 @@
-$(document).ready(function(){
-    $("btnLogin").kendoButton();
-})
-
-// $("#header").kendoAppBar({
-//     items: [
-//         {template: "<img id='logo' src='/images/logo.png' alt='Logo'>", type: "contentItem", className: "headerLeft"},
-//         {template: "<span> logout </span>", type: "contentItem", className: "headerRight"},
-//     ],
-//     positionMode: 'sticky',
-//     position: 'top',
-//     themeColor: 'light',
-// })
-
 kendo.init('#btnLogin');
