db81b41c10c04d23f28b981cb617a3534e14cd61
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-21 13:41:27 +0200

    registered user is inserted into db

--- src/main/java/com/samuraiDigital/adminsystem/data/model/UserInfo.java
+++ src/main/java/com/samuraiDigital/adminsystem/data/model/UserInfo.java
@@ -4,8 +4,6 @@ import java.sql.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
@@ -14,7 +12,6 @@ import javax.persistence.PrimaryKeyJoinColumn;
 public class UserInfo {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String surname;
