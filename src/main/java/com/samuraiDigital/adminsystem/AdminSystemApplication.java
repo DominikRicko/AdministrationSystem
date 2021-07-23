7de9ca3a36460abdcb273de499c9c822ab5c3a10
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-23 09:24:27 +0200

    header changes

--- src/main/java/com/samuraiDigital/adminsystem/AdminSystemApplication.java
+++ src/main/java/com/samuraiDigital/adminsystem/AdminSystemApplication.java
@@ -3,10 +3,8 @@ package com.samuraiDigital.adminsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 @EnableJpaRepositories("com.samuraiDigital.adminsystem.data.repositories")
@@ -15,9 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class AdminSystemApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(AdminSystemApplication.class, args);
-		//PasswordEncoder encoder = (PasswordEncoder)ctx.getBean("passwordEncoder");
-		//System.out.println(encoder.encode("abcdefgh"));
+		SpringApplication.run(AdminSystemApplication.class, args);
 	}
 
 }
