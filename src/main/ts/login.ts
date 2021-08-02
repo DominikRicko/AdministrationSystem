f2895ee44b5c78ce16e96399d9d329aff0433223
Dominik Ričko <dominik.ricko@gmail.com>
2021-08-02 14:19:18 +0200

    fixed formatting on frontend

--- src/main/ts/login.ts
+++ src/main/ts/login.ts
@@ -1,31 +1,31 @@
 $("#formLogin").kendoForm({
-    validatable: { 
-        validateOnBlur: true,
-        validationSummary: true,
-        errorTemplate: "Error",
-    },
-    orientation: "vertical",
-    formData:{
-        username: "",
-        password: "",
-    },
-    items: [
-        {
-            field: "username",
-            label: "Username: ",
-            validation: { required: true }
-        },
-        {
-            field: "password",
-            label: "Password: ",
-            validation: { required: true }
-        }
-    ],
-    buttonsTemplate: `
-    <button class="k-button k-primary k-form-submit" type="submit">Login</button>
-    <a href="/reset_password"><button class="k-button" type="button">I forgot my password</button></a>
-    <a href="/register"><button class="k-button" type="button">Register</button></a>
-    `
+	validatable: { 
+		validateOnBlur: true,
+		validationSummary: true,
+		errorTemplate: "Error",
+	},
+	orientation: "vertical",
+	formData:{
+		username: "",
+		password: "",
+	},
+	items: [
+		{
+			field: "username",
+			label: "Username: ",
+			validation: { required: true }
+		},
+		{
+			field: "password",
+			label: "Password: ",
+			validation: { required: true }
+		}
+	],
+	buttonsTemplate: `
+	<button class="k-button k-primary k-form-submit" type="submit">Login</button>
+	<a href="/reset_password"><button class="k-button" type="button">I forgot my password</button></a>
+	<a href="/register"><button class="k-button" type="button">Register</button></a>
+	`
 });
 
 $("#password").attr("type","password");
--- src/main/ts/users.ts
+++ src/main/ts/users.ts
@@ -1,157 +1,159 @@
 
-function parseDate(input : any){
-    for(const key in input){
+function parseDate(input: any) {
+	for (const key in input) {
 
-        if(input[key] instanceof Date){
+		if (input[key] instanceof Date) {
 
-            const day = (input[key] as Date).getDate();
-            (input[key] as Date).setUTCHours(0,0,0,0);
-            (input[key] as Date).setDate(day);
-            input[key] = (input[key] as Date).toISOString();
-        }
-    }
+			const day = (input[key] as Date).getDate();
+			(input[key] as Date).setUTCHours(0, 0, 0, 0);
+			(input[key] as Date).setDate(day);
+			input[key] = (input[key] as Date).toISOString();
+		}
+	}
 
-    console.log(input);
+	console.log(input);
 
-    return JSON.stringify(input);
+	return JSON.stringify(input);
 }
 
-function checkEmail(val : string) {
+function checkEmail(val: string) {
 
-    var re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    if (val == "") {
-        return false;
-    } else if (val.search(re) == -1) {
-        return false;
-    }
-    return true;
+	var re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+	if (val == "") {
+		return false;
+	} else if (val.search(re) == -1) {
+		return false;
+	}
+	return true;
 }
 
 $("#grid").kendoGrid({
-    columns:[
-        {
-            field: 'name',
-            width: '14%',
-            title: 'Name',
-            minResizableWidth: 70,
-        },
-        {
-            field: 'surname',
-            title: 'Surname',
-            width: '14%',
-            minResizableWidth: 70,
-        },
-        {
-            field: 'birthdate',
-            title: 'Birthdate',
-            width: '14%',
-            format: "{0:dd.MM.yyyy}",
-            minResizableWidth: 70,
-        },
-        {
-            field: 'groups',
-            title: 'Groups',
-            width: '14%',
-            minResizableWidth: 70,
-        },
-        {
-            field: 'email',
-            title: 'Email',
-            width: '25%',
-            minResizableWidth: 150,
-        },
-        {
-            field: 'username',
-            title: 'Username',
-            width: '25%',
-            minResizableWidth: 150,
-        },
-        {
-            field: 'enabled',
-            title: 'Enable',
-            width: '10%',
-            minResizableWidth: 60,
-        },
-        {
-            field: 'account_expiration_date',
-            title: 'Account Expires at',
-            width: '14%',
-            format: "{0:dd.MM.yyyy}",
-            minResizableWidth: 70,
-        },
-        {
-            field: 'credentials_expiration_date',
-            title: 'Credentials Expire at',
-            width: '14%',
-            format: "{0:dd.MM.yyyy}",
-            minResizableWidth: 70,
-        },
-        {
-            title: 'Commands',
-            width: '20%',
-            //@ts-ignore
-            command:["destroy"],
-            minResizableWidth: 120,
-        }
-    ],
-    dataSource: {
-        transport:{
-           read: {
-                url: "/i/api/v1/users/",
-                data: {
-                    format: "json"
-                },
-                type: "GET"
-            },
-            create: {
-                url: "/i/api/v1/users/",
-                type: "POST",
-                data: (input : any) => parseDate(input),
-                dataType: "json",
-                
-            },
-            destroy: {
-                url: (data : any) => "/i/api/v1/users/" + data.id,
-                type: "DELETE",
-                
-            },
-            update: {
-                url: (data : any) => "/i/api/v1/users/" + data.id,
-                data: (input : any) => parseDate(input),
-                dataType: "json",
-                type: "PUT",
-            },
-            batch: true,
-       },
-       schema: {
-           model: {
-               id: 'id',
-               fields: {
-                   id: {type: 'string'},
-                   name: {type: 'string'},
-                   surname: {type: 'string'},
-                   birthdate: {type: 'date'},
-                   groups: {type: 'string'},
-                   email: {
-                        type: 'string',
-                        validation: {
-                            required: true,
-                            customRule: function (input : any) {
-                                return checkEmail(input.val());
-                        }}},
-                   username: {type: 'string'},
-                   enabled: {type: 'boolean'},
-                   account_expiration_date: {type: 'date'},
-                   credentials_expiration_date: {type: 'date'},
-               }
-           },
-       }
-    },
-    scrollable: true,
-    selectable: true,
-    toolbar: ["create", "save", "cancel", "search" ],
-    editable: true,
-    filterable: true,
-    groupable: true,
-    sortable: true,
+	columns: [
+		{
+			field: 'name',
+			width: '14%',
+			title: 'Name',
+			minResizableWidth: 70,
+		},
+		{
+			field: 'surname',
+			title: 'Surname',
+			width: '14%',
+			minResizableWidth: 70,
+		},
+		{
+			field: 'birthdate',
+			title: 'Birthdate',
+			width: '14%',
+			format: "{0:dd.MM.yyyy}",
+			minResizableWidth: 70,
+		},
+		{
+			field: 'groups',
+			title: 'Groups',
+			width: '14%',
+			minResizableWidth: 70,
+		},
+		{
+			field: 'email',
+			title: 'Email',
+			width: '25%',
+			minResizableWidth: 150,
+		},
+		{
+			field: 'username',
+			title: 'Username',
+			width: '25%',
+			minResizableWidth: 150,
+		},
+		{
+			field: 'enabled',
+			title: 'Enable',
+			width: '10%',
+			minResizableWidth: 60,
+		},
+		{
+			field: 'account_expiration_date',
+			title: 'Account Expires at',
+			width: '14%',
+			format: "{0:dd.MM.yyyy}",
+			minResizableWidth: 70,
+		},
+		{
+			field: 'credentials_expiration_date',
+			title: 'Credentials Expire at',
+			width: '14%',
+			format: "{0:dd.MM.yyyy}",
+			minResizableWidth: 70,
+		},
+		{
+			title: 'Commands',
+			width: '20%',
+			//@ts-ignore
+			command: ["destroy"],
+			minResizableWidth: 120,
+		}
+	],
+	dataSource: {
+		transport: {
+			read: {
+				url: "/i/api/v1/users/",
+				data: {
+					format: "json"
+				},
+				type: "GET"
+			},
+			create: {
+				url: "/i/api/v1/users/",
+				type: "POST",
+				data: (input: any) => parseDate(input),
+				dataType: "json",
+
+			},
+			destroy: {
+				url: (data: any) => "/i/api/v1/users/" + data.id,
+				type: "DELETE",
+
+			},
+			update: {
+				url: (data: any) => "/i/api/v1/users/" + data.id,
+				data: (input: any) => parseDate(input),
+				dataType: "json",
+				type: "PUT",
+			},
+			batch: true,
+		},
+		schema: {
+			model: {
+				id: 'id',
+				fields: {
+					id: { type: 'string' },
+					name: { type: 'string' },
+					surname: { type: 'string' },
+					birthdate: { type: 'date' },
+					groups: { type: 'string' },
+					email: {
+						type: 'string',
+						validation: {
+							required: true,
+							customRule: function (input: any) {
+								return checkEmail(input.val());
+							}
+						}
+					},
+					username: { type: 'string' },
+					enabled: { type: 'boolean' },
+					account_expiration_date: { type: 'date' },
+					credentials_expiration_date: { type: 'date' },
+				}
+			},
+		}
+	},
+	scrollable: true,
+	selectable: true,
+	toolbar: ["create", "save", "cancel", "search"],
+	editable: true,
+	filterable: true,
+	groupable: true,
+	sortable: true,
 })
